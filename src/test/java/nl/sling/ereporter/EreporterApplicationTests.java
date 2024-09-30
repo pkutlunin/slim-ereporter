f7ba3becc5432d02199563f2b8ef0f69e60575b3
Pavel Kutlunin <kutlunin.pavel@gmail.com>
2024-09-30 17:35:30 +0200

    Initialize project
    - Spring Boot Initialized
    - add `.idea` folder to gitignore

---
+++ src/test/java/nl/sling/ereporter/EreporterApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.sling.ereporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EreporterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
